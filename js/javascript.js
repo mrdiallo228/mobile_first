9f8de30b8de20b32e2d2ebc1809b6914df73636e
DIALLO <mohamed.diallo@ccicampus.fr>
2025-01-08 12:49:11 +0100

    ajout des autres pages et et il ne rest que le js pour finir

--- js/javascript.js
+++ js/javascript.js
@@ -1,22 +1,14 @@
-let currentIndex = 0;
 
-  function showSlide(index) {
-    const carousel = document.getElementById('carousel');
-    const totalItems = document.querySelectorAll('.carousel-item').length;
-    if (index >= totalItems) {
-      currentIndex = 0;
-    } else if (index < 0) {
-      currentIndex = totalItems - 1;
-    } else {
-      currentIndex = index;
-    }
-    carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
-  }
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+  event.preventDefault(); // Empêche l'envoi par défaut
+  const reason = document.getElementById("reason").value;
 
-  function nextSlide() {
-    showSlide(currentIndex + 1);
+  if (reason === "") {
+    alert("Veuillez sélectionner une raison pour votre prise de contact !");
+  } else {
+    alert("Votre message a été envoyé avec succès !");
+    // Ici, on peut ajouter une fonction pour envoyer les données au serveur
   }
+});
 
-  function prevSlide() {
-    showSlide(currentIndex - 1);
-  }
+/*----GESTION DU MENU GURGER------*/
