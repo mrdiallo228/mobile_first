731ef2f0b9eb4ee4918cae26041b47dc8f93aa84
DIALLO <mohamed.diallo@ccicampus.fr>
2025-01-09 23:12:59 +0100

    menu burger actif pour la page contact

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,4 @@
-
+ 
 document.getElementById("contactForm").addEventListener("submit", function (event) {
   event.preventDefault(); // Empêche l'envoi par défaut
   const reason = document.getElementById("reason").value;
@@ -12,3 +12,19 @@ document.getElementById("contactForm").addEventListener("submit", function (even
 });
 
 /*----GESTION DU MENU GURGER------*/
+var sidenav = document.getElementById("mySidenav");
+var openBtn = document.getElementById("openBtn");
+var closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  sidenav.classList.add("active");
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  sidenav.classList.remove("active");
+}
